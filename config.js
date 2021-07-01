2c797ebcdcf9def07bdcc361b3fc3e21e55a75b3
leduykha13 <86778792+leduykha13@users.noreply.github.com>
2021-07-01 18:15:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho cậu <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h tớ qua chở cậu đi khách sạn nhóa =)).',
+    messDesc: 'Tối nay 7h tớ qua chở cậu đi khách sạn nhóa=)).',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100029468761276
 }
